1fcaa687252b70d880338a21e6d341670013e1ac
joaoprabelomf <joaoprabelomf@gmail.com>
2023-01-01 13:30:07 -0300

    Ultimo teste do teste, vamos aprender a mexer com isso

---
+++ app.js
@@ -0,0 +1,4 @@
+console.log("Rodando o sistema de gerenciamnto de jogos")
+console.log("Aplicação está se conectando ao banco de dados")
+// mais um console log de teste
+console.log("teste de commit")
